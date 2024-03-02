64e69d5f702cf8db44b00c1cb95f80cdfc4fecd1
mohammad <mghaneh1375@yahoo.com>
2024-03-02 10:12:48 +0330

    add visiblility option for drug in drug repository

--- src/main/java/four/group/jahadi/Repository/DrugRepository.java
+++ src/main/java/four/group/jahadi/Repository/DrugRepository.java
@@ -9,12 +9,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DrugRepository extends MongoRepository<Drug, ObjectId>, FilterableRepository<Drug> {
 
-  @Query(value = "{_id: {$in: ?0}}", fields = "{ 'name': 1, 'howToUse': 1, 'description': 1 }")
+  @Query(value = "{ _id: {$in: ?0}}", fields = "{ 'name': 1, 'howToUse': 1, 'description': 1 }")
   List<Drug> findByIds(List<ObjectId> ids);
 
-  @Query(value = "{name:{ $regex: ?0, $options:'i'}}", fields = "{ 'name': 1, '_id': 1 }")
+  @Query(value = "{ name: { $regex: ?0, $options:'i'}}", fields = "{ 'name': 1, '_id': 1 }", sort = "{'priority': 1}")
   List<Drug> findLikeName(String name);
   
-  @Query(fields = "{ 'name': 1, '_id': 1 }")
+  @Query(fields = "{ 'name': 1, '_id': 1 }", sort = "{'priority': 1}")
   List<Drug> findAllDigests();
+  
+  @Query(value = "{ name: { $regex: ?0, $options:'i'}, visibility: true }", fields = "{ 'name': 1, '_id': 1 }", sort = "{'priority': 1}")
+  List<Drug> findLikeNameAndVisible(String name);
+  
+  @Query(value = "{ visibility: true }", fields = "{ 'name': 1, '_id': 1 }", sort = "{'priority': 1}")
+  List<Drug> findVisibleDigests();
 }
