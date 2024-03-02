e2ef667acb3d09e387aa5fc88c8e01e5603d73d4
mohammad <mghaneh1375@yahoo.com>
2024-03-02 09:46:31 +0330

    add findByIds method in drug repository

--- src/main/java/four/group/jahadi/Repository/DrugRepository.java
+++ src/main/java/four/group/jahadi/Repository/DrugRepository.java
@@ -3,7 +3,12 @@ package four.group.jahadi.Repository;
 import four.group.jahadi.Models.Drug;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DrugRepository extends MongoRepository<Drug, ObjectId>, FilterableRepository<Drug> {}
+public interface DrugRepository extends MongoRepository<Drug, ObjectId>, FilterableRepository<Drug> {
+
+  @Query(value = "{_id: {$in: ?0}}", fields = "{ 'name': 1, 'howToUse': 1, 'description': 1 }")
+  List<Drug> findByIds(List<ObjectId> ids);
+}
