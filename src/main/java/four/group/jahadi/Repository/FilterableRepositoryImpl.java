d7a964f062ed3fd0b8d8505aaf215af21cf78a79
mghaneh1375 <mghaneh1375@gmail.com>
2025-04-29 22:16:32 +0330

    Fix timezone difference problem(mongodb and java) by using instant.

--- src/main/java/four/group/jahadi/Repository/FilterableRepositoryImpl.java
+++ src/main/java/four/group/jahadi/Repository/FilterableRepositoryImpl.java
@@ -1,11 +1,8 @@
 package four.group.jahadi.Repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,22 +14,9 @@ public class FilterableRepositoryImpl<T> implements FilterableRepository<T> {
     private MongoTemplate mongoTemplate;
 
     @Override
-    public Page<T> findAllWithFilter(Class<T> typeParameterClass, Filtering filtering, Pageable pageable) {
-        Query query = constructQueryFromFiltering(filtering).with(pageable);
-        List<T> ts = mongoTemplate.find(query, typeParameterClass);
-        return PageableExecutionUtils.getPage(ts, pageable, () -> mongoTemplate.count(query, typeParameterClass));
-    }
-
-    @Override
     public List<T> findAllWithFilter(Class<T> typeParameterClass, Filtering filtering) {
         Query query = constructQueryFromFiltering(filtering);
         return mongoTemplate.find(query, typeParameterClass);
-    }
-
-    @Override
-    public List<Object> getAllPossibleValuesForFilter(Class<T> typeParameterClass, Filtering filtering, String filterKey) {
-        Query query = constructQueryFromFiltering(filtering);
-        return mongoTemplate.query(typeParameterClass).distinct(filterKey).matching(query).all();
     }
 }
 
