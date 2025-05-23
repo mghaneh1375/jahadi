08e791fd3f8ee28c073e5477ef4604d549bda7e4
mghaneh1375 <mghaneh1375@gmail.com>
2025-05-23 13:59:57 +0330

    Accept persian format date or long timestamp for startAt and endAt in setRunInfo service.

--- src/main/java/four/group/jahadi/DTO/Region/ConvertStringToLongDeserialization.java
+++ src/main/java/four/group/jahadi/DTO/Region/ConvertStringToLongDeserialization.java
@@ -6,8 +6,7 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
 
-import static four.group.jahadi.Utility.Utility.convertJalaliToGregorianDate;
-import static four.group.jahadi.Utility.Utility.persianDateTimePattern;
+import static four.group.jahadi.Utility.Utility.*;
 
 public class ConvertStringToLongDeserialization extends JsonDeserializer<Long> {
 
@@ -15,16 +14,19 @@ public class ConvertStringToLongDeserialization extends JsonDeserializer<Long> {
     public Long deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         if(jsonParser == null) return null;
         try {
-            return jsonParser.getValueAsLong();
+            return jsonParser.getLongValue();
         }
         catch (Exception x) {
-            String value = jsonParser.getValueAsString();
-            if(!persianDateTimePattern.matcher(value.toString()).matches())
+            String value = jsonParser.getValueAsString().replace(" ", "").split("-")[0].replace("/", "-");
+            if(
+                    !persianDateTimePattern.matcher(value).matches() &&
+                    !persianDateTimePattern2.matcher(value).matches() &&
+                    !datePattern.matcher(value).matches() &&
+                    !datePattern2.matcher(value).matches()
+            )
                 throw new RuntimeException("field is not valid");
 
-            return convertJalaliToGregorianDate(
-                    value.split("-")[0].replace(" ", "").replace("/", "-")
-            ).getTime();
+            return convertJalaliToGregorianDate(value).getTime();
         }
     }
 }
